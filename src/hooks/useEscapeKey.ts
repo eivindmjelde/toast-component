fdc716ea75debc9c2f8cafce741da2425d5ba3c0
Eivind Mjelde <eivindmjelde@gmail.com>
2023-08-18 14:43:34 +0200

    create useEscapeKey hook

---
+++ src/hooks/useEscapeKey.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export function useEscapeKey(callback) {
+  useEffect(() => {
+    function handleEscKeyPress(e) {
+      if (e.code === "Escape") {
+        callback();
+      }
+    }
+    window.addEventListener("keydown", handleEscKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleEscKeyPress);
+    };
+  }, [callback]);
+}
